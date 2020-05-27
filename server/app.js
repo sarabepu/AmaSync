2a53305c3812066b3e0d0d10de7266b43eef4f50
Sara Bejarano <sm.bejarano44@uniandes.edu.co>
2020-05-27 18:47:17 -0500

    test cors

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const configPassport = require("./utils/passport-config");
 const cors = require('cors');
 
 const app = express();
-app.use(cors());
+//app.use(cors());
 
 app.use(logger("dev"));
 app.use(express.json());
