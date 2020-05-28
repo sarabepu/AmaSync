23be1509029e8a9b706634eb4ca49ca4a9d2c1da
Mariana <m.rodriguez21@uniandes.edu.co>
2020-05-27 23:29:23 -0500

    Save hashed passwords

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 const db = require("../utils/db");
 
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 // Register a new user
 router.post("/new", (req, res) => {
   let user = req.body.user;
-  db.createOne("users", user).then((user) => {
-    res.send(user);
-  });
+  let encrypt = user.password;
+  bcrypt
+    .hash(encrypt, saltRounds)
+    .then((hash) => {
+      user.password = hash;
+      return user;
+    })
+    .then((user) => {
+      db.createOne("users", user).then((user) => {
+        res.send(user);
+      });
+    });
 });
 
 module.exports = router;
